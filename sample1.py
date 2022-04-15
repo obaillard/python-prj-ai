adfcfe48d3d3d1a82c5964f1b3bc6ae9953079ef
obaillard <30605466+obaillard@users.noreply.github.com>
2022-04-15 17:25:05 +0200

    Add files via upload
    Upload some dummy file

---
+++ sample1.py
@@ -0,0 +1,8 @@
+def add(number1, number2):
+    """Returns sum of given numbers"""
+    return number1 + number2
+
+NUM1 = 4
+NUM2 = 5
+total = add(NUM1, NUM2)
+print("The sum of {} and {} is {}".format(NUM1, NUM2, total))
